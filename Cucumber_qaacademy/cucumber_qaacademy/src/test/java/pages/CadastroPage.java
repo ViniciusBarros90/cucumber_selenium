66cae38bc6e0ea5e3f68799f55cb7963c25b9ed0
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2023-06-27 15:17:55 -0300

    primeiro projeto Cucumber java -junit

---
+++ Cucumber_qaacademy/cucumber_qaacademy/src/test/java/pages/CadastroPage.java
@@ -0,0 +1,56 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CadastroPage {
+
+    static WebDriver driver;
+    static String REGISTRAR = "//button[contains(text(),'Registrar')]";
+    static String EMAIL =         "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[2]/form[1]/div[2]/input[1]";
+    static String NOME =          "//input[@name='name']";
+    static String SENHA =         "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[2]/form[1]/div[4]/div[1]/input[1]";
+    static String CONFIRM_SENHA = "//input[@name='passwordConfirmation']";
+    static String CRIAR_SALDO =   "//label[@id='toggleAddBalance']";
+    static String CADASTRAR =     "//button[contains(text(),'Cadastrar')]";
+    static String FECHAR =        "//*[@id=\"btnCloseModal\"]";
+
+    public CadastroPage(WebDriver driverParametro){ //Método construtor, onde consigo fazer todas as ações utilizando o Webdriver
+        this.driver = driverParametro;
+    }
+
+    public static void clicarRegistrar(){
+        driver.findElement(By.xpath(REGISTRAR)).click();
+
+    }
+    public static void PreencherEmail(String email) {
+        driver.findElement(By.xpath(EMAIL)).sendKeys(email);
+    }
+
+    public static void PreencherNome(String nome) {
+        driver.findElement(By.xpath(NOME)).sendKeys(nome);
+    }
+
+    public static void PreencherSenha(String senha) {
+        driver.findElement(By.xpath(SENHA)).sendKeys(senha);
+    }
+
+    public static void PreencherConfirmacaoSenha(String ConfirmacaoSenha) {
+        driver.findElement(By.xpath(CONFIRM_SENHA)).sendKeys(ConfirmacaoSenha);
+    }
+
+    public static void ClicarEmCriarComSaldo() {
+        driver.findElement(By.xpath(CRIAR_SALDO)).click();
+    }
+
+    public static void ClicarCadastrar() {
+        driver.findElement(By.xpath(CADASTRAR)).click();
+    }
+
+       public static void clicarFechar() {
+        driver.findElement(By.xpath(FECHAR)).click();
+}
+
+
+
+}
---
+++ Cucumber_qaacademy/cucumber_qaacademy/src/test/java/pages/LoginPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+
+public class LoginPage {
+        WebDriver driver;
+
+        String EMAIL = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/input[1]";
+        String SENHA = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[1]/form[1]/div[2]/div[1]/input[1]";
+        String ACESSAR = "//button[contains(text(),'Acessar')]";
+
+        public LoginPage(WebDriver driverVindoDoTeste) {// método construtor
+            this.driver = driverVindoDoTeste; // driver local passa a apontar para o driverVindoDoTeste
+        }
+
+        public void preencherEmail(String email) {
+            driver.findElement(By.xpath(EMAIL)).sendKeys(email);
+        }
+
+        public void preencherSenha(String senha) {
+            driver.findElement(By.xpath(SENHA)).sendKeys(senha);
+        }
+
+               public void clicarAcessar() {
+            driver.findElement(By.xpath(ACESSAR)).click();
+        }
+
+    }
+
